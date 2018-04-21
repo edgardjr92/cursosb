77b7f5831a9c37198a01b9fab8e68a5959b0e9dc
Edgard Jr <santoos.ed@gmail.com>
2018-04-21 15:05:23 -0300

    Banco de dados H2

--- src/main/java/com/edgardjr/cursosb/resources/CategoriaResource.java
+++ src/main/java/com/edgardjr/cursosb/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.edgardjr.cursosb.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edgardjr.cursosb.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String testar() {
-		return "Testando Rest";
+	public List<Categoria> testar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informatica");
+		Categoria categoria2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 
 }
