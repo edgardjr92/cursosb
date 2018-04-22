5e35d9432b50f951081d0dcfdf6fb1ed8e6d8955
Edgard Jr <santoos.ed@gmail.com>
2018-04-22 01:10:08 -0300

    criando operacao de instanciacao

--- src/main/java/com/edgardjr/cursosb/CursosbApplication.java
+++ src/main/java/com/edgardjr/cursosb/CursosbApplication.java
@@ -1,12 +1,31 @@
 package com.edgardjr.cursosb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.edgardjr.cursosb.domain.Categoria;
+import com.edgardjr.cursosb.repostories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursosbApplication {
+public class CursosbApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritórios");
+		
+		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 }
