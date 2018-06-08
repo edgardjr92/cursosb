a94a3b565483c143962473046c8e5816dd867392
Edgard Barbosa <edgard.barbosa@cortex-intelligence.com>
2018-06-08 18:00:30 -0300

    implementacao s3Service

---
+++ src/main/java/com/edgardjr/cursosb/services/S3Service.java
@@ -0,0 +1,38 @@
+package com.edgardjr.cursosb.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+	
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3Client;
+	
+	@Value("${s3.bucket}")
+	private String bucket;
+	
+	public void uploadFile(String path) {
+		try {
+			File file = new File(path);
+			this.s3Client.putObject(new PutObjectRequest(this.bucket, "teste", file));
+		} catch (AmazonServiceException e) {
+			LOG.info("AmazonServiceException: " + e.getErrorMessage());
+			LOG.info("Status coide: " + e.getErrorCode());
+		} catch (AmazonClientException e) {
+			LOG.info("AmazonClientException: " + e.getMessage());
+		} 
+	}
+}
